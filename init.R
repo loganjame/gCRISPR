834dbb844765aeb5d55f5c553666537393a0c0f5
loganjame <160886912+loganjame@users.noreply.github.com>
2024-03-12 16:48:52 -0400

    Update init.R

--- init.R
+++ init.R
@@ -2,7 +2,7 @@
 #
 # Example R code to install packages if not already installed
 #
-my_packages = c("shiny", "ggplot2")
+my_packages = c("shiny", "ggplot2","ggrepel", "reshape2", "dplyr" , "stringr" , "gridExtra", "DT")
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
